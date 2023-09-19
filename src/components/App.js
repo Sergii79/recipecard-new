53e2535bc23d2a438cfccd57f9f5f7dc7e604116
Sergii79 <tsymbal.sergiy@gmail.com>
2023-09-19 08:29:21 +0100

    commit first

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import { RecipeList } from "./RecipeList";
+
+export const App = () => {
+  return (
+    <div>
+      <RecipeList items={[1, 2, 3, 4, 5]} />
+      React homework template 132310978
+    </div>
+  );
+};
