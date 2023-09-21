587efc833ab8aff6293acc417e43172374b051cb
Sergii79 <tsymbal.sergiy@gmail.com>
2023-09-21 15:26:42 +0100

    commit second

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
  body {
@@ -13,5 +14,26 @@ export const GlobalStyle = createGlobalStyle`
 code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+ul,
+ol {
+  margin: 0;
+  padding: 0;
+  list-Style: none;
+}
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
 }
 `;
