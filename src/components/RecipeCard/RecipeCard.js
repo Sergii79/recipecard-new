4c792b6149217f52c7283e48c0479eb2b53bbc7d
Sergii79 <tsymbal.sergiy@gmail.com>
2023-09-28 16:05:04 +0100

    commit thid

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -21,6 +21,17 @@ import {
 import { ImageModal } from 'components/ImageModal/ImageModal';
 
 export class RecipeCard extends Component {
+  static propTypes = {
+    onDelete: PropTypes.func.isRequired,
+    item: PropTypes.shape({
+      image: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      time: PropTypes.number.isRequired,
+      servings: PropTypes.number.isRequired,
+      calories: PropTypes.number.isRequired,
+      difficulty: PropTypes.oneOf(['easy', 'medium', 'hard']).isRequired,
+    }).isRequired,
+  };
   state = {
     selectedImg: null,
   };
@@ -104,15 +115,3 @@ export class RecipeCard extends Component {
     );
   }
 }
-
-RecipeCard.propTypes = {
-  onDelete: PropTypes.func.isRequired,
-  item: PropTypes.shape({
-    image: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    time: PropTypes.number.isRequired,
-    servings: PropTypes.number.isRequired,
-    calories: PropTypes.number.isRequired,
-    difficulty: PropTypes.oneOf(['easy', 'medium', 'hard']).isRequired,
-  }).isRequired,
-};
